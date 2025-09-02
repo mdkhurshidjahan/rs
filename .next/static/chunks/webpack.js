8339e7a53a8be13aeb7fa85b8249959ef0910a9d
Md. Khurshid Jahan <73737980+mdkhurshidjahan@users.noreply.github.com>
2025-09-02 10:11:11 +0600

    Added _events.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b9af14444bef2d4"; }
+/******/ 		__webpack_require__.h = function() { return "a7f808f8eaad2f19"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
